d30519f7ac0339b25858d41baee8fa5af8db9788
ogendogen <kosmos500@interia.pl>
2020-02-23 14:27:23 +0100

    Sending keep alive command more frequently

--- ts3bot.py
+++ ts3bot.py
@@ -7,6 +7,9 @@ import sys
 import traceback
 import threading
 
+# Global TS3 handler object
+ts3conn = None
+
 def parse_cfg(file_name):
     with open(file_name) as file:
         lines = file.read().split("\n")
@@ -25,6 +28,11 @@ def report_status():
     sql_manager.report_status()
     threading.Timer(300.0, report_status).start()
 
+def keep_bot_alive():
+    global ts3conn
+    ts3conn.send_keepalive()
+    threading.Timer(60.0, keep_bot_alive).start()
+
 def update_admin_clid(admins, admin_id, clid):
     counter = 0
     for admin in admins:
@@ -35,50 +43,51 @@ def update_admin_clid(admins, admin_id, clid):
     return admins
 
 def start_bot(host, login, password, sid, sql_manager, group_ids):
-    with ts3.query.TS3Connection(host) as ts3conn:
-            ts3conn.login(
-                    client_login_name=login,
-                    client_login_password=password
-            )
-            ts3conn.use(sid=sid)
+    global ts3conn
+    ts3conn = ts3.query.TS3Connection(host)
+    ts3conn.login(
+            client_login_name=login,
+            client_login_password=password
+    )
+    ts3conn.use(sid=sid)
 
-            # Register for events
-            ts3conn.servernotifyregister(event="server")
+    # Register for events
+    ts3conn.servernotifyregister(event="server")
 
-            admins = sql_manager.get_admins()
+    admins = sql_manager.get_admins()
 
-            report_status()
+    report_status()
+    keep_bot_alive()
 
-            while True:
-                    ts3conn.send_keepalive()
-                    event = ts3conn.wait_for_event()
+    while True:
+            event = ts3conn.wait_for_event()
 
-                    # Client connected
-                    if event[0]["reasonid"] == "0":
-                        name = event[0]["client_nickname"]
-                        clid = int(event[0]["clid"])
-                        client_groups = event[0]["client_servergroups"].split(",")
-                        uid = event[0]["client_unique_identifier"]
-                        print(f"{str(datetime.now())} Client {name} connected")
+            # Client connected
+            if event[0]["reasonid"] == "0":
+                name = event[0]["client_nickname"]
+                clid = int(event[0]["clid"])
+                client_groups = event[0]["client_servergroups"].split(",")
+                uid = event[0]["client_unique_identifier"]
+                print(f"{str(datetime.now())} Client {name} connected")
 
-                        if len(intersection(group_ids, client_groups)) > 0: # If user is in any admin group
-                            admin_id = [admin["admin_id"] for admin in admins if admin["admin_uid"] == uid] # Try to get admin id from admins
-                            if len(admin_id) == 0: # Admin not registered
-                                admin_id = sql_manager.add_new_admin(name, uid, clid) # Register new admin
-                                admins.append({"admin_id": admin_id, "admin_name": name, "admin_uid": uid, "admin_clid": clid})
-                            else:
-                                admin_id = admin_id[0] # Admin already registered
-                                admins = update_admin_clid(admins, admin_id, clid)
-                                sql_manager.update_admin_clid(admin_id, clid)
+                if len(intersection(group_ids, client_groups)) > 0: # If user is in any admin group
+                    admin_id = [admin["admin_id"] for admin in admins if admin["admin_uid"] == uid] # Try to get admin id from admins
+                    if len(admin_id) == 0: # Admin not registered
+                        admin_id = sql_manager.add_new_admin(name, uid, clid) # Register new admin
+                        admins.append({"admin_id": admin_id, "admin_name": name, "admin_uid": uid, "admin_clid": clid})
+                    else:
+                        admin_id = admin_id[0] # Admin already registered
+                        admins = update_admin_clid(admins, admin_id, clid)
+                        sql_manager.update_admin_clid(admin_id, clid)
 
-                            sql_manager.save_admin_login(admin_id, int(time.time()))
+                    sql_manager.save_admin_login(admin_id, int(time.time()))
 
-                    # Client disconnected
-                    elif event[0]["reasonid"] == "8":
-                        clid = int(event[0]["clid"])
-                        admin_id = [admin["admin_id"] for admin in admins if admin["admin_clid"] == clid] # Check if it's admin
-                        if len(admin_id) > 0: # If admin then save
-                            sql_manager.save_admin_logout(admin_id, int(time.time()))
+            # Client disconnected
+            elif event[0]["reasonid"] == "8":
+                clid = int(event[0]["clid"])
+                admin_id = [admin["admin_id"] for admin in admins if admin["admin_clid"] == clid] # Check if it's admin
+                if len(admin_id) > 0: # If admin then save
+                    sql_manager.save_admin_logout(admin_id, int(time.time()))
 
 if __name__ == "__main__":
     try:
